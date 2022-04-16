b113a80e92bdc462b53731b799324e16ba5ebada
nicholasreimer <nchlsreimer@gmail.com>
2022-04-16 19:46:04 +0000

    modified the forked/cloned file to use OO programing principles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* ORIGINAL CODE:
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
@@ -21,11 +22,94 @@ function logTaskState(taskIndex) {
   console.log(`${title} has${complete ? " " : " not "}been completed`);
 }
 
-// DRIVER CODE BELOW
-
+// ORIGINAL DRIVER CODE BELOW
 newTask("Clean Cat Litter"); // task 0
 newTask("Do Laundry"); // task 1
 
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+*/
+
+//-----------------------------------------------------------------------------
+//NEW CODE #1:
+/*
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  };
+  return task;
+}
+
+// prints out the provided task's details
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
+}
+
+// DRIVER CODE BELOW
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
+console.log(tasks);
+*/
+//-----------------------------------------------------------------------------
+// NEW CODE #2:  FUNCTIONS BECOME METHODS WITHIN THE OBJECT
+
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+}
+
+//Accessing properties or methods of our tasks is as simple as writing:
+task1.title
+task1.markCompleted();
+
+// DRIVER CODE CHANGES BELOW
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+//-----------------------------------------------------------------------------
+/*  SUMMARY:
+-We are using objects to group state and logic in order to keep the code more
+organized. Now everything associated to a task has been encapsulated inside the
+task object.
+
+-the func newTask takes two args var title and var description which are both
+properties within an object named task which is within the funcs body.
+The newTask func outputs the value of the task object as its func output
+
+-within the func is an object called task that contains a series of key value pairs
+
+-also within the func is a method (a func within a func) called logState that
+prints out a concateted string to the console if called
+
+-below the method while still inside the original func is another merthod called
+markCompleted in its body ther is code that when called will change the boolean
+value of the original funcs object property "complete:" to TRUE
+
+*/
